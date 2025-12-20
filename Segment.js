1850d44b8d13974d3e0062d3029ed1e2f20734a4
Viktor Ádám Trefiman <varazsgoreny765@gmail.com>
2025-12-20 01:29:26 +0100

    Adding description to Segment.

--- Segment.js
+++ Segment.js
@@ -1,7 +1,17 @@
+import { Bodypart } from './Bodypart.js';
+import { SegmentDescriptor } from './Descriptor.js';
 import Point from './Point.js';
 
+/** Decribes a segment of a creature. */
 export default class Segment {
-	constructor(origin, distanceFromPrev, skinRadius, bodyparts) {
+	/**
+	 * Creates a Segment object.
+	 * @param {Point} origin The position of the segment.
+	 * @param {number} distanceFromPrev The distance of this segment from the previous one.
+	 * @param {number} skinRadius The width of the creature at this segment.
+	 * @param {Bodypart[]} bodyparts The additional bodyparts.
+	 */
+	constructor(origin, distanceFromPrev, skinRadius, bodyparts = null) {
 		this.origin = origin;
 		this.distanceFromPrev = distanceFromPrev;
 		this.skinRadius = skinRadius;
@@ -18,6 +28,7 @@ export default class Segment {
 		this.nextSegment = null;
 	}
 
+	/** Iterates through the segments. */
 	*[Symbol.iterator]() {
 		let current = this;
 		while (current) {
@@ -26,14 +37,20 @@ export default class Segment {
 		}
 	}
 
-	static createAndLink(startingPoint, descriptors) {
-		const result = descriptors[0].create(startingPoint);
+	/**
+	 * Creates the segments by the given descriptors and links them together.
+	 * @param {Point} startingPoint The starting poition of the first segment.
+	 * @param {SegmentDescriptor[]} segmentDescriptors The descriptors of the segments.
+	 * @returns The head segment of the linked list.
+	 */
+	static createAndLink(startingPoint, segmentDescriptors) {
+		const result = segmentDescriptors[0].create(startingPoint);
 
 		let current = result;
 		let next;
 
-		for (let index = 1; index < descriptors.length; ++index) {
-			next = descriptors[index].create(current.origin);
+		for (let index = 1; index < segmentDescriptors.length; ++index) {
+			next = segmentDescriptors[index].create(current.origin);
 
 			current.nextSegment = next;
 			next.prevSegment = current;
@@ -46,6 +63,13 @@ export default class Segment {
 		return result;
 	}
 
+	/**
+	 * Pulls the given neighbour segment torwards this segment.
+	 * @param {Segment} segment The segment to pull torwards.
+	 * @param {Segment} segmentToPull The segment to pull.
+	 * @param {number} distanceBetween The needed distance in between the two segments.
+	 * @param {Segment} segmentInFront The segment on the other side of the main segment.
+	 */
 	static pull(segment, segmentToPull, distanceBetween, segmentInFront = null) {
 		const fromSegmentToNext = Point.subtract(segmentToPull.origin, segment.origin);
 		let toJoinPoint = Point.scale(fromSegmentToNext, distanceBetween);
@@ -56,6 +80,10 @@ export default class Segment {
 		segmentToPull.origin = Point.add(segment.origin, toJoinPoint);
 	}
 
+	/**
+	 * Pulls the next segment of the given segment.
+	 * @param {Segment} segment The given segment.
+	 */
 	static pullNext(segment) {
 		if (segment.nextSegment instanceof Segment) {
 			Segment.pull(segment, segment.nextSegment, segment.nextSegment.distanceFromPrev, segment.prevSegment);
@@ -64,6 +92,10 @@ export default class Segment {
 		}
 	}
 
+	/**
+	 * Pulls the previous segment of the given segment.
+	 * @param {Segment} segment The given segment.
+	 */
 	static pullPrev(segment) {
 		if (segment.prevSegment instanceof Segment) {
 			Segment.pull(segment, segment.prevSegment, segment.distanceFromPrev, segment.nextSegment);
@@ -72,6 +104,12 @@ export default class Segment {
 		}
 	}
 
+	/**
+	 * Gets the front vector of the given segment.
+	 * @param {Segment} segment The given segment.
+	 * @throws If the segment has no neighbours.
+	 * @returns The calculated vector.
+	 */
 	static getFrontVector(segment) {
 		let prev = segment.prevSegment;
 		let next = segment.nextSegment;
@@ -87,6 +125,11 @@ export default class Segment {
 		return Point.scale(vector, segment.skinRadius);
 	}
 
+	/**
+	 * Gets the outline points of the segment list.
+	 * @param {Segment} headSegment The head segment.
+	 * @returns The outline points.
+	 */
 	static getPoints(headSegment) {
 		const roundNoseAngle = radians(45);
 
@@ -121,6 +164,11 @@ export default class Segment {
 		return left.reverse().concat(right);
 	}
 
+	/**
+	 * Draws all the bodyparts of the given segment set to the given render mode.
+	 * @param {Segment} segment The segment with the bodyparts.
+	 * @param {boolean} render The render mode.
+	 */
 	static drawBodyparts(segment, render) {
 		if (segment.bodyparts instanceof Array) {
 			for (const bodypart of segment.bodyparts) {
@@ -130,6 +178,13 @@ export default class Segment {
 		}
 	}
 
+	/**
+	 * Restricts the given direction vector by the vector created between the two segments and the angle properties of the first segment.
+	 * @param {Segment} firstSegment The first segment.
+	 * @param {Segment} secondSegment The second segment.
+	 * @param {Point} direction The vector to restrict.
+	 * @returns The restricted vector.
+	 */
 	static restrictAngleOfRotation(firstSegment, secondSegment, direction) {
 		const fromSecondToFirst = Point.subtract(firstSegment.origin, secondSegment.origin);
 
